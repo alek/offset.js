ba1099f3597e3653715efb437db0eb31f824ea37
alek <alek.br@gmail.com>
2020-05-21 14:07:05 -0700

    cleanup

--- offset.js
+++ offset.js
@@ -20,11 +20,11 @@
     BlockDiagram.init = function(el, params) {
         var self = this;
         self.container = document.getElementById(el);
-        self.anchor = {x: params['x'], y: params['y']};
-        self.width = params['width'];
-        self.height = params['height'];
-        self.blockMap = {}
-        self.objectIDs = []
+        self.anchor = {x: params.x, y: params.y};
+        self.width = params.width;
+        self.height = params.height;
+        self.blockMap = {};
+        self.objectIDs = [];
     }
     
     BlockDiagram.prototype = {
@@ -48,16 +48,16 @@
 		},
         getCoord: function(params) {
         	if (this.grid) {
-				if ((params['x'] != null) && (params['y'] != null)) {
-			        return {'x': params['x']*(this.grid['cellWidth'] + this.grid['rowGutter']),
-			        		'y': params['y']*(this.grid['cellHeight'] + this.grid['columnGutter'])
+				if ((params.x != null) && (params.y != null)) {
+			        return {x: params.x*(this.grid.cellWidth + this.grid.rowGutter),
+			        		y: params.y*(this.grid.cellHeight + this.grid.columnGutter)
 		    	    	}
 				} else {
 					throw "Grid coordinates missing."
 				}
     	    } else {
-		        return {'x': this.width*Math.random(),
-		        		'y': this.height*Math.random()
+		        return {x: this.width*Math.random(),
+		        		y: this.height*Math.random()
 	    	    }
 	    	}
         },
@@ -66,9 +66,9 @@
         		return val;
         	} else {
         		if (type === 'width') {
-        			return val*(this.grid['cellWidth'] + this.grid['rowGutter'])
+        			return val*(this.grid.cellWidth + this.grid.rowGutter)
         		} else if (type === 'height') {
-        			return val*(this.grid['cellHeight'] + this.grid['columnGutter'])
+        			return val*(this.grid.cellHeight + this.grid.columnGutter)
         		} else {
         			throw "Uknown type"
         		}
@@ -79,7 +79,7 @@
         },
         getSVGFontStyle: function(params) {
 			var config = {
-				"font-size": params['fontSize'] ? params['fontSize'] : (this.fontSize ? this.fontSize : 10),
+				"font-size": params.fontSize ? params.fontSize : (this.fontSize ? this.fontSize : 10),
 				"text-align": "middle",
 				"alignment-baseline": "middle",
 				"text-anchor": "middle",
@@ -92,7 +92,7 @@
         },
         getHTMLFontStyle: function(params, width, height) {
 			var config = {
-				"font-size": params['fontSize'] ? params['fontSize'] : (this.fontSize ? this.fontSize : 10),
+				"font-size": params.fontSize ? params.fontSize : (this.fontSize ? this.fontSize : 10),
 				"font-family": "Helvetica;sans-serif",
 				"display": "flex",
 				"align-items": "center",
@@ -133,11 +133,11 @@
         },
         findObjects: function(params) {
         	var results = []        
-        	if (params["start"] && params["end"]) {
-        		results.push(document.getElementById(this.getObjectID("path", [params["start"],params["end"]])))
+        	if (params.start && params.end) {
+        		results.push(document.getElementById(this.getObjectID("path", [params.start,params.end])))
 			} else {
-				results.push(document.getElementById(this.getObjectID("text", params["title"])))	
-        		results.push(document.getElementById(this.getObjectID("block", params["title"])));
+				results.push(document.getElementById(this.getObjectID("text", params.title)));
+        		results.push(document.getElementById(this.getObjectID("block", params.title)));
 			}
 			return results;
         },
@@ -156,11 +156,11 @@
         	if (this.grid) {
         		var delta = [[1,0],[-1,0], [0,1], [0,-1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
         		for (var i=0; i<delta.length; i++) {
-                    var xPos = coord['x']+delta[i][0]
-                    var yPos = coord['y']+delta[i][1]
+                    var xPos = coord.x+delta[i][0]
+                    var yPos = coord.y+delta[i][1]
                     if (this.isWithinBounds({x: xPos, y: yPos})) {
-	        		     var gridX = Math.floor(xPos/(this.grid.cellWidth+this.grid.columnGutter))
-	        		     var gridY = Math.floor(yPos/(this.grid.cellHeight+this.grid.rowGutter))
+	        		     var gridX = Math.floor(xPos/(this.grid.cellWidth+this.grid.columnGutter));
+	        		     var gridY = Math.floor(yPos/(this.grid.cellHeight+this.grid.rowGutter));
 	        		     if (this.grid.matrix[gridX][gridY]) {
 	        			    return true;
     	        		 }
@@ -173,30 +173,30 @@
         	}
         },
         isWithinBounds: function(coord) {
-        	return (coord['x'] > 0) && (coord['y'] > 0) && (coord['x'] < this.width) && (coord['y'] < this.height)        	
+        	return (coord.x > 0) && (coord.y > 0) && (coord.x < this.width) && (coord.y < this.height)        	
         },
         getFreeNeighbors: function(coord) {
         	var candidates = [
-        		{'x': coord['x'] + this.grid.cellWidth/2, 'y': coord['y']},
-        		{'x': coord['x'] - this.grid.cellWidth/2, 'y': coord['y']},
-        		{'x': coord['x'], 'y': coord['y'] + this.grid.cellHeight/2},
-        		{'x': coord['x'], 'y': coord['y'] - this.grid.cellHeight/2},
+        		{x: coord.x + this.grid.cellWidth/2, y: coord.y},
+        		{x: coord.x - this.grid.cellWidth/2, y: coord.y},
+        		{x: coord.x, y: coord.y + this.grid.cellHeight/2},
+        		{x: coord.x, y: coord.y - this.grid.cellHeight/2},
         	]
         	var result = []
         	for (var i=0; i<candidates.length; i++) {
         		if (!this.isOccupied(candidates[i]) && this.isWithinBounds(candidates[i])) {
-        			result.push(candidates[i])
+        			result.push(candidates[i]);
         		} 
         	}
-        	return result
+        	return result;
         },
         getDistance: function(node1, node2) {
-        	return Math.sqrt(Math.pow(node1['x'] - node2['x'],2) + Math.pow(node1['y'] - node2['y'],2))
+        	return Math.sqrt(Math.pow(node1.x - node2.x,2) + Math.pow(node1.y - node2.y,2));
         },
         distanceSort: function(target) {
         	return function(a, b) {
-        		var d1 = Math.sqrt(Math.pow(a['x'] - target['x'],2) + Math.pow(a['y'] - target['y'],2))
-        		var d2 = Math.sqrt(Math.pow(b['x'] - target['x'],2) + Math.pow(b['y'] - target['y'],2))
+        		var d1 = Math.sqrt(Math.pow(a.x - target.x,2) + Math.pow(a.y - target.y,2));
+        		var d2 = Math.sqrt(Math.pow(b.x - target.x,2) + Math.pow(b.y - target.y,2));
         		return d2 - d1
         	}
         },
@@ -206,26 +206,26 @@
 			var visited = {}
 
 			while(queue.length > 0) {
-				var el = queue.pop()
-				path.push(el)
-				var distance = this.getDistance(el, endNode)
+				var el = queue.pop();
+				path.push(el);
+				var distance = this.getDistance(el, endNode);
 				if (distance < this.grid.cellWidth/2) {
-					path.push(endNode)
-					break
+					path.push(endNode);
+					break;
 				}
-				var neigh = this.getFreeNeighbors(el)
-				neigh = neigh.sort(this.distanceSort(endNode))
+				var neigh = this.getFreeNeighbors(el);
+				neigh = neigh.sort(this.distanceSort(endNode));
 				for (var i=0; i<neigh.length; i++) {
 					if (!visited[this.getJSONHash(neigh[i])]) {
-						visited[this.getJSONHash(el)] = true
-						queue.push(neigh[i])
+						visited[this.getJSONHash(el)] = true;
+						queue.push(neigh[i]);
 					}
 				}
 				if (path.length > 100) {
-					break
+					break;
 				}
 			}
-			return path.map(function(el) { return [el['x'], el['y']] })
+			return path.map(function(el) { return [el.x, el.y] });
         },
         getRoute: function(startPads, endPads) {
 			var bestPath = null
@@ -244,22 +244,21 @@
 
         setGrid: function(config) {
         	this.grid = {
-        		rows: config['rows'],
-        		columns: config['columns'],
+        		rows: config.rows,
+        		columns: config.columns,
         		columnGutter: 0,
         		rowGutter: 0,
-        		cellHeight: this.height/config['rows'],
-        		cellWidth: this.width/config['columns'],
-        		matrix: this.getGridMatrix(config['columns'], config['rows'])
+        		cellHeight: this.height/config.rows,
+        		cellWidth: this.width/config.columns,
+        		matrix: this.getGridMatrix(config.columns, config.rows)
         	}
             return this;
         },
         updateGridAllocation: function(params) {
-            // console.log(params)
         	if (this.grid) {
-        		for (var x=params['x']; x < params['x'] + params['width']; x++) {
-        			for (var y=params['y']; y < params['y'] + params['height']; y++) {
-        				this.grid.matrix[x][y] = true
+        		for (var x=params.x; x < params.x + params.width; x++) {
+        			for (var y=params.y; y < params.y + params.height; y++) {
+        				this.grid.matrix[x][y] = true;
         			}
         		}
         	}
@@ -272,7 +271,7 @@
 				width: this.width,
 				height: this.height,
 				stroke: null,
-				fill: params['color'],
+				fill: params.color,
 				style: "stroke-width:0;"
 			});	
         	return this;
@@ -319,15 +318,15 @@
 			return this;
 		},
         addCircle: function(params) {
-	        var coord = this.getCoord(params)
+	        var coord = this.getCoord(params);
         	this.circle({
-        		cx: coord['x'],
-        		cy: coord['y'],
-				r: params['r'],
+        		cx: coord.x,
+        		cy: coord.y,
+				r: params.r,
 				stroke: null,
-				fill: (params['color'] != null) ? params['color'] : "#f00",
+				fill: (params.color != null) ? params.color : "#f00",
 				style: "stroke-width:0",
-				id: params['id']
+				id: params.id
 			});
         	return this;
         },
@@ -338,12 +337,12 @@
 
             while (!result) {
                 var coord = {
-                    x: Math.ceil(Math.random()*(1 + this.grid.columns - params['width'] - 2)),
-                    y: Math.ceil(Math.random()*(1 + this.grid.rows - params['height'] - 2))
+                    x: Math.ceil(Math.random()*(1 + this.grid.columns - params.width - 2)),
+                    y: Math.ceil(Math.random()*(1 + this.grid.rows - params.height - 2))
                 }
                 var freeSpace = true;
-                for (var i=0; i<params['width']; i++) {
-                    for (var j=0; j<params['height']; j++) {
+                for (var i=0; i<params.width; i++) {
+                    for (var j=0; j<params.height; j++) {
                         if (this.grid.matrix[coord.x + i][coord.y + j]) {
                             freeSpace = false;
                         }
@@ -355,7 +354,7 @@
                 }
                 
                 if (++attemptCount > 10) {
-                    throw "No space available";
+                    throw "No free space available";
                 }
             }
             
@@ -363,118 +362,118 @@
         },
         addBlock: function(params) {
 
-            var width = this.getMeasure(params['width'], 'width');
-            var height = this.getMeasure(params['height'], 'height');
+            var width = this.getMeasure(params.width, 'width');
+            var height = this.getMeasure(params.height, 'height');
 
-            if (params['x'] && params['y']) {
+            if (params.x && params.y) {
                 var coord = this.getCoord(params);
             } else {    
                 var target = this.placeBlock(params)
-                params['x'] = target['x'];
-                params['y'] = target['y'];
-                var coord = this.getCoord(target)
+                params.x = target.x;
+                params.y = target.y;
+                var coord = this.getCoord(target);
             }
 
             this.rect({
-                x: coord['x'],
-                y: coord['y'],
+                x: coord.x,
+                y: coord.y,
                 width: width,
                 height: height,
-                stroke: params['border'] ? params['border'] : this.color,
-                fill: params['fill'] ? params['fill'] : "none",
+                stroke: params.border ? params.border : this.color,
+                fill: params.fill ? params.fill : "none",
                 style: "stroke-width:1",
-                id: this.getObjectID("block", params['title'])
+                id: this.getObjectID("block", params.title)
             }); 
-            this.objectIDs.push(this.getObjectID("block", params['title']));
+            this.objectIDs.push(this.getObjectID("block", params.title));
             this.updateGridAllocation(params);
 
 			var fontSize = this.getFontSize(params);
 			if (params['title'].length*fontSize < width) {
 				this.text( { 
-					x: coord['x'] + width/2,
-					y: coord['y'] + height/2,
+					x: coord.x + width/2,
+					y: coord.y + height/2,
 					"fill": this.textColor ? this.textColor : "#fff",
 					"style": this.getSVGFontStyle(params),
-					id: this.getObjectID("text", params['title'])
-					}, params['title']); 
+					id: this.getObjectID("text", params.title)
+					}, params.title); 
 			} else {
 				this.textBlock( { 
-					x: coord['x'],
-					y: coord['y'],
+					x: coord.x,
+					y: coord.y,
 					width: width,
 					height: height,
-					"color": this.textColor ? this.textColor : "#fff",
-					"style": this.getHTMLFontStyle(params, width, height),
-					id: this.getObjectID("text", params['title'])
-					}, params['title']); 
+					color: this.textColor ? this.textColor : "#fff",
+					style: this.getHTMLFontStyle(params, width, height),
+					id: this.getObjectID("text", params.title)
+					}, params.title); 
 			}
-			this.objectIDs.push(this.getObjectID("text", params['title']));
+			this.objectIDs.push(this.getObjectID("text", params.title));
 
-        	this.blockMap[params["title"]] = {
-        		"coord": coord,
-        		"width": width,
-        		"height": height,
-        		"center": {'x': coord['x'] + width/2, 'y': coord['y'] + height/2},
-        		"pads": [ {'x': coord['x'] + width/2, 'y': coord['y']}, 
-        				  {'x': coord['x'] + width/2, 'y': coord['y'] + height},
-        				  {'x': coord['x'], 'y': coord['y'] + height/2}, 
-        				  {'x': coord['x'] + width, 'y': coord['y'] + height/2} ]
+        	this.blockMap[params.title] = {
+        		coord: coord,
+        		width: width,
+        		height: height,
+        		center: {x: coord.x + width/2, y: coord.y + height/2},
+        		pads: [ {x: coord.x + width/2, y: coord.y}, 
+        				{x: coord.x + width/2, y: coord.y + height},
+        				{x: coord.x, y: coord.y + height/2}, 
+        		      	{x: coord.x + width, y: coord.y + height/2} ]
         	}
 
         	return this;
         },
         addWire: function(params) {
         	var coords = []
-        	if (params["path"]) {	// explicit grid path 
+        	if (params.path) {	// explicit grid path 
         		if (this.grid) {
-        			for (var i=0; i<params["path"].length; i++) {
-        				var el = params["path"][i];
+        			for (var i=0; i<params.path.length; i++) {
+        				var el = params.path[i];
         				coords.push([this.getMeasure(el[0],"width"), this.getMeasure(el[1], "height")]);
         			}
         		} else {
-        			coords = params["path"];
+        			coords = params.path;
         		}
         	} else {	// autorouter
-	        	var start = this.blockMap[params["start"]]['pads'];
-	        	var end = this.blockMap[params["end"]]['pads'];
+	        	var start = this.blockMap[params.start].pads;
+	        	var end = this.blockMap[params.end].pads;
 	        	coords = this.getRoute(start, end)
 			}
 			this.path( {
 				d: "M" + coords.map(function(x) { return x.join(" ")}).join(" L") + "",
 				"stroke-width": 1,
-				"fill": "none",
-				"stroke": this.color ? this.color : "#fff",
-				id: this.getObjectID("path", [params["start"],params["end"]])
+				fill: "none",
+				stroke: this.color ? this.color : "#fff",
+				id: this.getObjectID("path", [params.start,params.end])
 			})
-			this.objectIDs.push(this.getObjectID("path", [params["start"],params["end"]]))
+			this.objectIDs.push(this.getObjectID("path", [params.start,params.end]))
 			return this;
         },
         enableBlock(params) {
-        	var el = document.getElementById(this.getObjectID("block", params["title"]));
+        	var el = document.getElementById(this.getObjectID("block", params.title));
         	var fill = el.getAttribute("fill");
         	el.setAttribute("fill", this.color);
-        	el.setAttribute("fill-opacity", params["opacity"] ? params["opacity"] : 1.0);
+        	el.setAttribute("fill-opacity", params.opacity ? params.opacity : 1.0);
         	return this;
         },
         disableBlock(params) {
-        	var el = document.getElementById(this.getObjectID("block", params["title"]));
+        	var el = document.getElementById(this.getObjectID("block", params.title));
         	var fill = el.getAttribute("fill");
 			el.setAttribute("fill", null);
         	return this;
         },
         toggleBlock(params) {
-        	var el = document.getElementById(this.getObjectID("block", params["title"]));
+        	var el = document.getElementById(this.getObjectID("block", params.title));
         	var fill = el.getAttribute("fill");
         	if (fill === "none") {
 	        	el.setAttribute("fill", this.color);
-	        	el.setAttribute("fill-opacity", params["opacity"] ? params["opacity"] : 1.0);
+	        	el.setAttribute("fill-opacity", params.opacity ? params.opacity : 1.0);
         	} else {
 	        	el.setAttribute("fill", null);
         	}
         	return this;
         },
         toggleWire(params) {
-        	var el = document.getElementById(this.getObjectID("path", [params["start"],params["end"]]));
+        	var el = document.getElementById(this.getObjectID("path", [params.start,params.end]));
         	var width = el.getAttribute("stroke-width");
         	if (width == 1) {
         		el.setAttribute("stroke-width", 2);
@@ -484,17 +483,17 @@
         showGrid(opacity) {
         	if (!opacity) { opacity = 1.0 }
         	if (this.grid) {
-        		for (var i=0; i<this.grid['columns']; i++) {
+        		for (var i=0; i<this.grid.columns; i++) {
 					this.line({
-						x1: i*(this.grid['cellWidth'] + this.grid['columnGutter']), y1: 0,
-						x2: i*(this.grid['cellWidth'] + this.grid['columnGutter']), y2: this.height,
+						x1: i*(this.grid.cellWidth + this.grid.columnGutter), y1: 0,
+						x2: i*(this.grid.cellWidth + this.grid.columnGutter), y2: this.height,
 						stroke: "rgba(255,255,255," + opacity + ")", "stroke-width": 1
 					});
         		}
-    			for (var i=0; i<this.grid['columns']; i++) {
+    			for (var i=0; i<this.grid.columns; i++) {
 					this.line({
-						x1: 0, y1: i*(this.grid['cellHeight'] + this.grid['rowGutter']), 
-						x2: this.width,  y2: i*(this.grid['cellHeight'] + this.grid['rowGutter']),
+						x1: 0, y1: i*(this.grid.cellHeight + this.grid.rowGutter), 
+						x2: this.width,  y2: i*(this.grid.cellHeight + this.grid.rowGutter),
 						stroke: "rgba(255,255,255," + opacity + ")", "stroke-width": 1
 					});
     			}
